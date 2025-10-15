5080bbea27d315cc348f95837d68783d9f2be4b3
Chirag-264 <rchirag264@gmail.com>
2025-10-15 19:20:41 +0530

    rock-paper-scissors

---
+++ stonePaperScissor/script.js
@@ -0,0 +1,57 @@
+let score = JSON.parse(localStorage.getItem('score'));
+    if (!score) {
+        score = {
+            wins: 0,
+            loss: 0,
+            ties: 0
+        }
+    }
+
+    updateScoreBoard();
+
+    let res = document.querySelector('.js-result');
+    let choice = document.querySelector('.js-choice');
+    
+
+    function Move() {
+        let computerMove = Math.random();
+        if(computerMove >= 0 && computerMove < 1/3) randomMove = 'Rock';
+        else if(computerMove >= 1/3 && computerMove < 2/3) randomMove = 'Paper';
+        else randomMove = 'Scissors';
+        return randomMove;
+    }
+    function resultGame(user) {
+        random = Move();
+        result = '';
+        if (user === 'Rock') {
+            if(random === 'Rock') result = 'Tie';
+            else if(random === 'Paper') result = 'Wou Lose';
+            else result = 'You Win';
+        }
+        if (user === 'Paper') {
+            if(random === 'Rock') result = 'You Win';
+            else if(random === 'Paper') result = 'Tie';
+            else result = 'You Lose';
+        }
+        if (user === 'Scissors') {
+            if(random === 'Rock') result = 'You Lose';
+            else if(random === 'Paper') result = 'You Win';
+            else result = 'Tie';
+        }
+
+        if(result === 'You Win') score.wins += 1;
+        else if(result === 'You Lose') score.loss += 1;
+        else score.ties += 1;
+
+        res.innerHTML = `${result}`;
+        choice.innerHTML = `You <img src="assets/${user}-emoji.png"> - <img src="assets/${random}-emoji.png"> Computer`;
+        updateScoreBoard();
+
+        const score1 = JSON.stringify(score);
+        localStorage.setItem('score', score1);
+    }
+
+    function updateScoreBoard() {
+        let scoreBoard = document.querySelector('.js-score');
+        scoreBoard.innerHTML = `Wins: ${score.wins}. Losses: ${score.loss}. Ties: ${score.ties}`;
+    }
